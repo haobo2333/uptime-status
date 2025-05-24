72366f64db545143fde93fab8a11411a2ac500e4
haobo2333 <point1145@outlook.com>
2025-05-24 21:36:23 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,17 +1,19 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'Public Status',
+  SiteName: 'Server Status',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
     'm800598649-ee1b244d9fe4eb534d2e7a26',
     'm800598647-dc48362c67cea3e34de25a2e',
+    'm800600310-700eac9f2578cf4b8408a242',
+    'm800600290-3868380491e2812d9ad049c2',
   ],
 
   // 日志天数
-  CountDays: 90,
+  CountDays: 30,
 
   // 是否显示检测站点的链接
   ShowLink: true,
@@ -20,15 +22,11 @@ window.Config = {
   Navi: [
     {
       text: 'Homepage',
-      url: 'https://status.org.cn/'
-    },
-    {
-      text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      url: 'https://status.touxl.top/'
     },
     {
       text: 'Blog',
-      url: 'https://abo.xyz/'
+      url: 'https://www.touxl.top'
     },
   ],
 };
