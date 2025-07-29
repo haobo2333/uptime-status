21233a5d907b2a479bbcf58bc95a54dc279635d3
haobo2333 <point1145@outlook.com>
2025-07-29 17:09:04 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -6,9 +6,8 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm800598647-dc48362c67cea3e34de25a2e',
-    'm800600310-700eac9f2578cf4b8408a242',
     'm800600290-3868380491e2812d9ad049c2',
+    'm800600310-700eac9f2578cf4b8408a242',
     'm801025677-75909b6f45d0e0ec61045e5a'
   ],
 
